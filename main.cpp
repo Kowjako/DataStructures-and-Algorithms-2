e099c72e23b55e29e1293ac3a1f0c66ea274cd61
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-05-14 19:43:29 +0200

    Create testing menu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-#include <matrixgraph.h>
-#include <listgraph.h>
-#include <binaryheap.h>
+#include "matrixgraph.h"
+#include "listgraph.h"
+#include "binaryheap.h"
 #include <windows.h>
 #include <iomanip>
 
@@ -24,22 +24,57 @@ int main()
 
     /* Nie zapominac o setDirect dla algorytmow MST oraz sciezki */
 
-    //matrix.createRandomGraph(5, false, 10);
+   // listg.readFromFile(tmp);
 
-    matrix.createRandomGraph(5,false,10);
-
-    matrix.print();
-    matrix.kruskalAlg();
-    /* double sum = 0;
-    for(int i=0;i<50;i++) {
-        matrix.createRandomGraph(50, false, 306);
-
+   // listg.dijkstraAlg(0);
+    double sum = 0;
+    int vertex = 2000;
+    for(int i=0;i<2;i++) {
+        int maxEdges = vertex*(vertex-1)/2;
+        listg.createRandomGraph(vertex, true, (int)(maxEdges*0.25));
+        cout<<"Graph 25 created"<<endl;
         start = read_QPC();
-        matrix.kruskalAlg();
+        listg.dijkstraAlg(0);
         elapsed = read_QPC() - start;
         sum+= elapsed;
     }
-    cout << "Time [us] = "<< (1000000.0 * sum) /(frequency*50) << endl; */
+    cout << "Time [ms] 25% = "<< (1000.0 * sum) /(frequency*2) << endl;
+
+    sum = 0;
+    for(int i=0;i<2;i++) {
+        int maxEdges = vertex*(vertex-1)/2;
+        listg.createRandomGraph(vertex, true, (int)(maxEdges*0.5));
+     cout<<"Graph 50 created"<<endl;
+        start = read_QPC();
+        listg.dijkstraAlg(0);
+        elapsed = read_QPC() - start;
+        sum+= elapsed;
+    }
+    cout << "Time [ms] 50% = "<< (1000.0 * sum) /(frequency*2) << endl;
+
+    sum = 0;
+    for(int i=0;i<2;i++) {
+        int maxEdges = vertex*(vertex-1)/2;
+        listg.createRandomGraph(vertex, true, (int)(maxEdges*0.75));
+ cout<<"Graph 75 created"<<endl;
+        start = read_QPC();
+        listg.dijkstraAlg(0);
+        elapsed = read_QPC() - start;
+        sum+= elapsed;
+    }
+    cout << "Time [ms] 75% = "<< (1000.0 * sum) /(frequency*2) << endl;
+
+    sum = 0;
+    for(int i=0;i<2;i++) {
+        int maxEdges = vertex*(vertex-1)/2;
+        listg.createRandomGraph(vertex, true, (int)(maxEdges*0.99));
+ cout<<"Graph 99 created"<<endl;
+        start = read_QPC();
+        listg.dijkstraAlg(0);
+        elapsed = read_QPC() - start;
+        sum+= elapsed;
+    }
+    cout << "Time [ms] 99% = "<< (1000.0 * sum) /(frequency*2) << endl;
     return 0;
 }
 
