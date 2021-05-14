fc10a0ea3ab6d0df353bd1b3be94161d4e830c13
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-05-14 19:42:22 +0200

    Corrected algorithm, change 10000 as Infinity

--- listgraph.cpp
+++ listgraph.cpp
@@ -45,6 +45,11 @@ void ListGraph::clear() {
         this->head[i] = nullptr;
     }
     delete[] this->head;
+
+    for(int i=0;i<this->node_num ;i++)
+        delete[] this->edgeMacierz[i];
+    delete[] this->edgeMacierz;
+
     this->head = nullptr;
     this->node_num = 0;
     this->edge_num = 0;
@@ -208,8 +213,8 @@ void ListGraph::dijkstraAlg(int start) {
 
     /* Poczatkowa inicjaliacja */
     for(int i=0;i<this->node_num;i++) {
-        d[i] = 10000;
-        p[i] = 10000;
+        d[i] = INT_MAX;
+        p[i] = INT_MAX;
         validated[i] = false;
     }
     d[start] = 0;
@@ -247,13 +252,13 @@ void ListGraph::dijkstraAlg(int start) {
     }
 
     /* Wyswietlenie wyniku z wykorzystaniem stosu*/
-    stack<int> roads;
+    /* stack<int> roads;
     cout<<"Start = "<<start<<endl;
     for(int i = 1;i<this->node_num;i++) {
         cout<<"To: "<<i<<" Dist: "<<d[i]<<" Path: ";
         int address = i;
         roads.push(i);
-        while(p[address]!=10000) {
+        while(p[address]!=INT_MAX) {
             roads.push(p[address]);
             address = p[address];
         }
@@ -265,7 +270,7 @@ void ListGraph::dijkstraAlg(int start) {
             roads.pop();
         }
         cout<<endl;
-    }
+    } */
     /* Zwolnienie pamieci */
     delete[] validated;
     delete[] p;
@@ -303,17 +308,17 @@ int ListGraph::getWeight(int start, int finish) {
             return startNode->weight;
         startNode = startNode->next;
     }
+    return 0;
 }
 
 void ListGraph::bellmanFordAlg(int start) {
-    cout<<"edge num = "<<getEdgeNumber()<<endl;
     int counter = 0;
     int* edge = new int[2];
     int* d = new int[this->node_num];  /*tablica aktualnych odleglosci*/
     int* p = new int[this->node_num];  /*tablica koncowych wierzcholkow*/
     for(int i=0;i<this->node_num;i++) {
-        d[i] = 10000;
-        p[i] = 10000;
+        d[i] = INT_MAX-100;
+        p[i] = INT_MAX;
     }
     d[start] = 0;
 
@@ -337,19 +342,19 @@ void ListGraph::bellmanFordAlg(int start) {
     }
 
     /*Wyswietlanie wyniku*/
-    stack<int> roads;
+   /** stack<int> roads;
     cout<<"Start = "<<start<<endl;
     for(int i = 1;i<this->node_num;i++) {
         cout<<"To: "<<i<<" Dist: "<<d[i]<<" Path: ";
         int address = i;
         roads.push(i);
         /*Odkladamy na stosie az do poczatkowego bo poprzednik poczatkowego to 10000*/
-        while(p[address]!=10000) {
+   /*     while(p[address]!=INT_MAX) {
             roads.push(p[address]);
             address = p[address];
         }
         /*Pobieramy ze stosu i wyswietlamy */
-        while(!roads.empty()) {
+   /*     while(!roads.empty()) {
             if(roads.size()==1)
                 cout<<roads.top()<<"";
             else
@@ -357,7 +362,7 @@ void ListGraph::bellmanFordAlg(int start) {
             roads.pop();
         }
         cout<<endl;
-    }
+    }  */
     /*Zwolnienie pamieci */
     delete edge;
     delete[] d;
@@ -372,11 +377,11 @@ void ListGraph::primAlg() {
     BinaryHeap bHeap;
 
     for(int i=0;i<this->node_num;i++) {
-        key[i] = 10000;
+        key[i] = INT_MAX;
         validated[i] = false;
     }
     key[0] = 0;
-    p[0] = 10000;
+    p[0] = INT_MAX;
 
     /*Inicjalizacja kolejki priorytetowej*/
     for(int i=0;i<this->node_num;i++)
@@ -409,13 +414,13 @@ void ListGraph::primAlg() {
         }
     }
 
-    /*Koncowe wyswietlanie*/
-    int sum = 0;
+    /** Koncowe wyswietlanie **/
+    /* int sum = 0;
     for(int i=1;i<this->node_num;i++) {
         cout<<"("<<p[i]<<";"<<i<<")"<<" -> "<<key[i]<<endl;
         sum+=key[i];
     }
-    cout<<"MST = "<<sum<<endl;
+    cout<<"MST = "<<sum<<endl; */
 
     /*Zwolnienie pamieci */
     delete[] validated;
@@ -424,7 +429,7 @@ void ListGraph::primAlg() {
     delete[] p;
 }
 
-void ListGraph::kruskalAlg() {
+void ListGraph::kruskalAlg(listEdge** sortedEdges) {
     bool* isEdgeMakeSolution = new bool[this->edge_num];
     for(int i=0;i<this->edge_num;i++) {
         isEdgeMakeSolution[i] = false;
@@ -433,7 +438,7 @@ void ListGraph::kruskalAlg() {
     listEdge* actualEdge = nullptr;
 
     /*Tworzenie posortowanej listy krawedzi*/
-    listEdge** edges = createSortedEdgesList();
+    listEdge** edges = sortedEdges;
 
     /* Tworzenie poddrzew */
     for(int i=0;i<this->node_num;i++) {
@@ -451,14 +456,14 @@ void ListGraph::kruskalAlg() {
         }
     }
     /*Wyswietlenie wyniku*/
-    int sum = 0;
+    /* int sum = 0;
     for(int i=0;i<this->edge_num;i++) {
         if(isEdgeMakeSolution[i]) {
             cout<<"("<<edges[i]->start<<";"<<edges[i]->finish<<")"<<" -> "<<edges[i]->weight<<endl;
             sum+=edges[i]->weight;
         }
     }
-    cout<<"MST = "<<sum<<endl;
+    cout<<"MST = "<<sum<<endl; */
     /*Zwolnienie pamieci*/
     delete[] isEdgeMakeSolution;
     delete[] grupa;
@@ -503,17 +508,19 @@ listEdge** ListGraph::createSortedEdgesList() {
 }
 
 void ListGraph::createRandomGraph(int vertexNumber, bool direction, int edgeNumber) {
+    clear();
     /* Maks. ilosc krawedzi to graf pelny wynosi n*(n-1)/2 a minimalna to n-1*/
     if(edgeNumber>(vertexNumber)*(vertexNumber-1)*0.5 || edgeNumber<vertexNumber-1 && !direction) {
         cout<<"Nie moze byc tyle krawedzi!"<<endl;
         return;
     }
     /* Usuniecie poprzedniego gdy istnial i inicjowanie nowego */
-    clear();
     /* Inicjowanie zmiennych poczatkowych */
     this->node_num = vertexNumber;
     this->directed = direction;
     this->edge_num = 0;
+
+    this->edgeMacierz = new int*[edgeNumber];
 
     this->head = new node*[vertexNumber];
     for(int i=0;i<this->node_num;i++) {
@@ -521,17 +528,29 @@ void ListGraph::createRandomGraph(int vertexNumber, bool direction, int edgeNumb
     }
 
     int maxEdgeNumber = (vertexNumber * vertexNumber - vertexNumber) / 2; /* graf pelny */
+    int edgeCounter = 0;
     /* Tworzenie grafu minimalnego = spojnego */
     for (int i=0; i<this->node_num-1;i++) { /* minimalna ilosc krawedzi = n - 1 aby graf byl spojny */
-        connect(i,i + 1,rand()%30+1);
+        connect(i,i + 1,rand()%(INT_MAX-200));
+        this->edgeMacierz[edgeCounter] = new int[2]; /*tworzymy miejsce na jedna krawedz */
+        this->edgeMacierz[edgeCounter][0] = i;
+        this->edgeMacierz[edgeCounter][1] = i+1;
+        edgeCounter++;
     }
     int i,j;
     /*Nastepnie tworzymy aby osiagnac zadana ilosc krawedzi */
     while(this->edge_num!=edgeNumber) {
-        i = rand()%vertexNumber;
-        j = rand()%vertexNumber;
-        if(getWeight(i,j)==0 && i!=j) {    /*jezeli nie ma polaczenia oraz wierzcholek nie wskazuje na siebie*/
-            connect(i,j,rand()%30+1);   /*polacz*/
+        for(int i=0;i<vertexNumber;i++) {
+            for(int j=0;j<vertexNumber;j++) {
+                if(this->edge_num==edgeNumber) return;
+                if(getWeight(i,j)==0 && i!=j) {    /*jezeli nie ma polaczenia oraz wierzcholek nie wskazuje na siebie*/
+                    connect(i,j,rand()%(INT_MAX-200));   /*polacz*/
+                    this->edgeMacierz[edgeCounter] = new int[2]; /*tworzymy miejsce na jedna krawedz */
+                    this->edgeMacierz[edgeCounter][0] = i;
+                    this->edgeMacierz[edgeCounter][1] = j;
+                    edgeCounter++;
+                }
+            }
         }
     }
 }
